8b95714aed15c16a4a7ea7579122397e03a938f9
brene04 <141616909+brene04@users.noreply.github.com>
2026-04-05 15:03:13 +1000

    Update axiosConfig.js

--- qts/src/axiosConfig.js
+++ qts/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.106.115.104:5001/api',
+  baseURL: 'http://16.176.231.168:5001/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
