24b538806105257c057e1ec669981e7f30b0c708
brene04 <141616909+brene04@users.noreply.github.com>
2026-04-16 00:18:52 +1000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://16.176.231.168'] }));
+app.use(cors({ origin: ['http://localhost:3000', 'http://3.107.27.130 '] }));
 app.use(express.json());
 
 app.use('/api/bookings', require('./routes/bookings'));
